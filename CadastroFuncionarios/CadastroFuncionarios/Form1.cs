b476de31cac6aeb8d317646b56f82b4e9eac72eb
joaohenriquers <91610605+joaohenriquers@users.noreply.github.com>
2021-09-29 11:24:23 -0300

    Projeto Cadastro de Funcionários

---
+++ CadastroFuncionarios/CadastroFuncionarios/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroFuncionarios
+{
+    public partial class Form1 : Form
+    {
+        dbfuncionariosEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db = new dbfuncionariosEntities();
+            funcionariosBindingSource.DataSource = db.Funcionarios.ToList();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            using(frmAdicionarEditarFuncionario frm = new frmAdicionarEditarFuncionario(null))
+            {
+                if (frm.ShowDialog() == DialogResult.OK)
+                    funcionariosBindingSource.DataSource = db.Funcionarios.ToList();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (funcionariosBindingSource.Current == null)
+                return;
+            using (frmAdicionarEditarFuncionario frm = new frmAdicionarEditarFuncionario(funcionariosBindingSource.Current as Funcionarios))
+            {
+                if (frm.ShowDialog() == DialogResult.OK)
+                    funcionariosBindingSource.DataSource = db.Funcionarios.ToList();
+            }
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            if (funcionariosBindingSource.Current != null)
+            {
+                if(MessageBox.Show("Tem certeza que deseja excluir este registro", "Mensagem", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    db.Funcionarios.Remove(funcionariosBindingSource.Current as Funcionarios);
+                    funcionariosBindingSource.RemoveCurrent();
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HoraExtra he = new HoraExtra();
+            he.ShowDialog();
+        }
+    }
+}
---
+++ CadastroFuncionarios/CadastroFuncionarios/HoraExtra.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroFuncionarios
+{
+    public partial class HoraExtra : Form
+    {
+        public HoraExtra()
+        {
+            InitializeComponent();
+        }
+
+        private void HoraExtra_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (dbfuncionariosEntities db = new dbfuncionariosEntities())
+                {
+                    cboFuncionarios.DataSource = db.Funcionarios.ToList();
+                    cboFuncionarios.ValueMember = "Salario";
+                    cboFuncionarios.DisplayMember = "NomeCompleto";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cboFuncionarios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            Funcionarios obj = cboFuncionarios.SelectedItem as Funcionarios;
+            if (obj != null)
+            {
+                txtSalario.Text = obj.Salario.ToString();
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void txtSalario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float calculo = 0;
+            float vhe = 0;
+            vhe = (float.Parse(txtSalario.Text) * 5)/100;
+            calculo = (float.Parse(txtHoraExtra.Text) * vhe) + float.Parse(txtSalario.Text); 
+            txtSalarioCheio.Text = calculo.ToString();
+        }
+    }
+}
